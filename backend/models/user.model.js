eb8bdf916fe7393f43af37787339e1582ec18afe
[ARNAV] <arnavgoyal1317@gmail.com>
2025-08-06 22:59:06 +0530

    Added Schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: [
+            "admin",
+            "reciever",
+            "publisher"
+        ],
+        default: "publisher",
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    collegeName: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    
+    securityKey: {
+        type: String,
+        default: "",
+        require: true,
+        minlength: 6,
+
+    }
+
+
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+
+
+export default User;
