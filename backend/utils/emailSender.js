2acc6d32ddd820af26e463f642f9492150aac3cb
Hitesh <hiteshkumarnain@gmail.com>
2025-08-11 18:50:33 +0530

    df

--- backend/utils/emailSender.js
+++ backend/utils/emailSender.js
@@ -1,34 +1,23 @@
-import nodemailer from "nodemailer";
+import { transporter } from "./nodemailer.js";
 
 export const mailSender = async (req, res) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false,
-      auth: {
-        user: "maddison53@ethereal.email", //user email
-        pass: "jn7jnAPss4f63QBp6D", //key
-      },
-    });
-
-    // jo email jaega uska object
     const mailOption = {
-      from: '"Dr. Tayyab Khan" <tayyab email>',
-      to: req.body.to, // receiver ka email from request
+      from: `"Dr. Tayyab Khan" <${process.env.EMAIL_USER}>`,
+      to: req.body.to,
       subject: "Dr. Tayyab Khan is inviting you to review an application",
-      text: "This is a test email",
-      html: "<b>Hello world?</b>",
-    }
+      text: "You are invited to review an application.",
+      html: `<b>Hello,</b><br>
+             You are invited to review an application.<br>
+             <a href="https://your-website.com/review?appId=${req.body.appId}">Click here to review</a>`,
+    };
 
     const info = await transporter.sendMail(mailOption);
 
     console.log("Message sent:", info.messageId);
-    console.log(info);
     res.status(200).json({ message: "Email sent", messageId: info.messageId });
   } catch (error) {
-    console.log("Error in sending mail through node mailer")
-    console.log(error.message);
-    res.status(502).json({message: "Email wasn't sent"})
+    console.log("Error in sending mail through node mailer");
+    res.status(502).json({ message: "Email wasn't sent" });
   }
 };
