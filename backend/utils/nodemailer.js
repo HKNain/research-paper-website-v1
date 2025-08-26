f582aac6f76dd09c8fe0d545f3689bd1c2308dd2
Hitesh <hiteshkumarnain@gmail.com>
2025-08-26 21:24:39 +0530

    nodemailer added

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -1,8 +1,13 @@
-import nodemailer from "nodemailer"
-export const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER, // Gmail
-        pass: process.env.EMAIL_PASS  // App Password
-      }
-    });
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export default transporter;
